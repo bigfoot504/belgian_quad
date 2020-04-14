83666e2dae6b13f8b1c444a494559ba2719b3a08
bigfoot504 <61797407+bigfoot504@users.noreply.github.com>
2020-04-14 15:52:10 -0400

    Add files via upload

---
+++ MountainCar_Scripts/mtncar_torch.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 10 15:04:08 2020
+
+@author: locker
+
+Me building a MountainCar solver from scratch using PyTorch.
+"""
+
+import gym
+import numpy as np
+from tqdm import tqdm
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+class Model(nn.Module):
+    # initializing class creates a sequential NN
+    def __init__(self):
+        super(Model, self).__init__()
+        self.fc1 = nn.Linear(2, 64)
+        self.fc2 = nn.Linear(64, 64)
+        self.fc3 = nn.Linear(64, 3)
+            
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+env = gym.make('MountainCar-v0')
+done = False
+best_so_far = [-200]
+EPISODES = 2000
+
+for episode in range(EPISODES):
+    env.reset()
+    actions = []
+    doneEp = False
+    
+    while not doneEp:
+        # get an action
+        if np.random.random() <= epsilon: # Explore
+            # get a random action
+            action = np.random.randint(0,3)
+        else:                             # Exploit
+            # use network to get an educated action
+            
+            
+        # take step
+        state, reward, doneEp, _ = env.step(actions[-1])
+        
+        # Make done episode only if at goal position
+        if state[0] < env.goal_position:
+            doneEp = False
+    
+
+    
