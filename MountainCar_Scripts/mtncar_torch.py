b0cef454184c4b6bb339d91de700361526763558
bigfoot504 <61797407+bigfoot504@users.noreply.github.com>
2020-04-20 14:43:31 -0400

    Update mtncar_torch.py

--- MountainCar_Scripts/mtncar_torch.py
+++ MountainCar_Scripts/mtncar_torch.py
@@ -12,6 +12,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
+from torch.autograd import Variable
 
 class Model(nn.Module):
     def __init__(self):
@@ -20,6 +21,7 @@ class Model(nn.Module):
         self.fc2 = nn.Linear(64, 64)
         self.fc3 = nn.Linear(64, 3)
             
+    # function from super that must be modified for each subclass
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
@@ -27,34 +29,81 @@ class Model(nn.Module):
         return x
 
 env = gym.make('MountainCar-v0')
-state = env.reset()
 model = Model()
-model(torch.from_numpy(state).float())
+tgt_model = model
+LEARNING_RATE = 0.001
+GAMMA = 0.99 # discount factor
+optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+scheduler = optim.lr_scheduler.StepLF(optimizer, step_size=1, gamma=GAMMA)
 
 done = False
 best_so_far = [-200]
 EPISODES = 2000
+# function to decay epsilon from 1 to 0 over EPISODES horizon
+epsilon = lambda episodes : 0#(EPISODES - episodes) / EPISODES
+UPDATE_TARGET_EVERY = 5
+UPDATE_STATS_EVERY = 50
+SHOW_EVERY = 1
+SHOW = False
+max_pos_ls = []
+
 
 for episode in range(EPISODES):
-    env.reset()
+    print(episode)
+    
+    state = env.reset() # reset current state
+    state = torch.from_numpy(state).float() # convert state np2torch
     actions = []
     doneEp = False
+    max_pos = -0.4
     
     while not doneEp:
+        if episode % SHOW_EVERY == 0 and SHOW == True:
+            env.render()
+        
+        # pass current torch state through NN
+        Qs = model(state)
+        
         # get an action
-        if np.random.random() <= epsilon: # Explore
+        if np.random.random() <= epsilon(episode): # Explore
             # get a random action
             action = np.random.randint(0,3)
         else:                             # Exploit
             # use network to get an educated action
+            maxQ, action = torch.max(Qs, -1)
+            action = action.item()
             
-            
-        # take step
-        state, reward, doneEp, _ = env.step(actions[-1])
+        # Take step
+        state_new, reward, doneEp, _ = env.step(action)
+        state_new = torch.from_numpy(state_new).float() # convert state np2torch
         
-        # Make done episode only if at goal position
-        if state[0] < env.goal_position:
-            doneEp = False
+        # pass action through tgt model to get tgt Q (& eventually max future Q)
+        tgt_Qs = tgt_model(state_new)
+        max_future_Q = reward + GAMMA * torch.max(tgt_Qs) # max future Q is reward + discounted tgt Q
+        
+        # compute loss & update model
+        loss_fn = nn.MSELoss()
+        loss = loss_fn(Qs, tgt_Qs)
+        model.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+        # update tgt model if it's time (maybe every 5?)
+        if episode % UPDATE_TARGET_EVERY == 0:
+            tgt_model = model
+        
+        # update stats
+        state = state_new
+        if state[0] > max_pos:
+            max_pos = state[0]
+        
+        # if episode is done
+        if doneEp:
+            scheduler.step()
+            
+    max_pos_ls.append(max_pos)
+        
+    env.close()
     
 
     
